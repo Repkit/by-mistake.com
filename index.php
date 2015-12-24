a31121d94f834fabb28b0eefb94c59227d387054
repkit <ionut.baches@yahoo.com>
2015-12-24 16:33:48 +0000

    first commit on index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Great ideas come by mistake!</title>
+    </head>
+    <body>
+        Hello, I'm here to teach you from my mistakes!
+    </body>
+</html>
