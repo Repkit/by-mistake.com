67776f462396dd2b03890206ca32102ab64317c9
repkit <ionut.baches@yahoo.com>
2015-12-25 10:13:07 +0000

    change title

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="dcsplus scheduled maintenance">
-    <meta name="author" content="dcsplus">
+    <meta name="description" content="The greatest ideas come by mistake!">
+    <meta name="author" content="by-mistake">
     <!-- <link rel="shortcut icon" href="ico/favicon.ico"> -->
 	<title>dcsplus scheduled maintenance</title>
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
