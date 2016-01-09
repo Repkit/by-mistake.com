2ec4b151587b060af6810085bd6eb994b38fa4e7
repkit <ionut.baches@yahoo.com>
2016-01-09 13:34:08 +0000

    create by mistake wall

--- index.php
+++ index.php
@@ -1,110 +1,121 @@
 <!DOCTYPE html>
-<html>
+<html data-language="en_US" lang="en">
 <head>
 	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="The greatest ideas come by mistake!">
-    <meta name="author" content="by-mistake">
-    <!-- <link rel="shortcut icon" href="ico/favicon.ico"> -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="The greatest ideas come by mistake!">
+  <meta name="author" content="by-mistake">
+  <link rel="icon" type="image/png" href="http://res.cloudinary.com/by-mistake-com/image/upload/v1452344232/logo.by-mistake.v3.2_32x32_fnk8fl.png">
 	<title>The greatest ideas come by mistake!</title>
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 
     <!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-	<link href='https://fonts.googleapis.com/css?family=Allura' rel='stylesheet' type='text/css'>
-	<!-- custom styles -->
-	<style type="text/css">
-		@media (min-width: 320px) {
-		  body{
-		  	background: url('http://res.cloudinary.com/by-mistake-com/image/upload/c_scale,h_320,w_480/a_90/v1451035454/wip-bg_vvwfel.jpg') no-repeat center center fixed;
-		  	-webkit-background-size: cover;
-		    -moz-background-size: cover;
-		    -o-background-size: cover;
-		    background-size: cover;
-			color:#fff;
-		  	background-color:#333;
-		  	font-family: 'Open Sans',Arial,Helvetica,Sans-Serif;
-		  	height: 100%;
-		  }
-		  p.blocktext {
-        margin-left: auto;
-        margin-right: auto;
-        width: 7em;
-        font-family: 'Allura', cursive;
-        font-size: 24px;
-        color: #919198;
-        margin-top: 21px;
-      }
-		}
-		@media (min-width: 480px) {
-		  body{
-		  	background: url('http://res.cloudinary.com/by-mistake-com/image/upload/c_scale,h_320,w_480/a_0/v1451035454/wip-bg_vvwfel.jpg') no-repeat center center fixed;
-		  	-webkit-background-size: cover;
-		    -moz-background-size: cover;
-		    -o-background-size: cover;
-		    background-size: cover;
-			  color:#fff;
-		  	background-color:#333;
-		  	font-family: 'Open Sans',Arial,Helvetica,Sans-Serif;
-		  	height: 100%;
-		  }
-		}
-
-		@media (min-width: 667px) {
-		  body{
-		  	background: url('http://res.cloudinary.com/by-mistake-com/image/upload/w_768/wip-bg_vvwfel.jpg') no-repeat center center fixed;
-		  	-webkit-background-size: cover;
-		    -moz-background-size: cover;
-		    -o-background-size: cover;
-		    background-size: cover;
-			color:#fff;
-		  	background-color:#333;
-		  	font-family: 'Open Sans',Arial,Helvetica,Sans-Serif;
-		  	height: 100%;
-		  }
-		}
-
-		@media (min-width: 992px) {
-		  body{
-		  	background: url('http://res.cloudinary.com/by-mistake-com/image/upload/w_992/wip-bg_vvwfel.jpg') no-repeat center center fixed;
-		  	-webkit-background-size: cover;
-		    -moz-background-size: cover;
-		    -o-background-size: cover;
-		    background-size: cover;
-			  color:#fff;
-		  	background-color:#333;
-		  	font-family: 'Open Sans',Arial,Helvetica,Sans-Serif;
-		  	height: 100%;
-		  }
-		  p.blocktext {
-        margin-left: auto;
-        margin-right: auto;
-        width: 7em;
-        font-family: 'Allura', cursive;
-        font-size: 57px;
-        color: #919198;
-      }
-		}
-	</style>
+	<link rel="stylesheet" href="css/wip.min.css" type="text/css" />
+  <link href='https://fonts.googleapis.com/css?family=Short+Stack' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<div class="content">
-	  <p class="blocktext">Work in progress...</p>
-	</div>
-
-<!-- scripts -->
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-	 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-	<script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
-<!-- end of scripts -->
+  <div class="brand-logo"></div>
+    <div class="wall-title">
+      <h1>While we are working hard to finish the website "by mistake" (alot) we've created "by-mistake wall".</h1>
+      <h2>Bear with us and... keep improving yourself from mistakes!</h2>
+      <a href="https://twitter.com/ideasbymistake" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @ideasbymistake</a>
+    </div>
+    <div class="sticky-note yellow">
+      <span class="pin"></span>
+      <div class="sticky-note-inner">
+        <p>To help yourself, you must be yourself. Be the best that you can be. When you make a <strong>mistake</strong>, learn from it, pick yourself up and move on.</p>
+        <span class="author">Dave Pelzer</span>
+      </div>
+    </div>
+    
+    <div class="sticky-note cyan">
+      <span class="tape"></span>
+      <div class="sticky-note-inner">
+        <p>I hope that in this year to come, you make <strong>mistakes</strong>. Because if you are making <strong>mistakes</strong>, then you are making new things, trying new things, learning, living, pushing yourself, changing yourself, changing your world. You're doing things you've never done before, and more importantly, you're doing something.</p>
+        <span class="author">Neil Gaiman</span>
+      </div>
+    </div>
+    
+    
+    <div class="sticky-note yellow">
+      <span class="pin"></span>
+      <div class="sticky-note-inner">
+        <p>If you live long enough, you'll make <strong>mistakes</strong>. But if you learn from them, you'll be a better person. It's how you handle adversity, not how it affects you. The main thing is never quit, never quit, never quit.</p>
+        <span class="author">William Jefferson Clinton</span>
+      </div>
+    </div>
+    
+    <div class="sticky-note yellow">
+      <span class="tape"></span>
+      <div class="sticky-note-inner">
+        <p>I'm selfish, impatient, and a little insecure. I make <strong>mistakes</strong>, I'm out of control, and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.</p>
+        <span class="author">Marilyn Monroe</span>
+      </div>
+    </div>
+    <div class="sticky-note orange">
+      <span class="tape"></span>
+      <div class="sticky-note-inner">
+        <p>A man must be big enough to admit his <strong>mistakes</strong>, smart enough to profit from them, and strong enough to correct them.</p>
+        <span class="author">John C. Maxwell</span>
+      </div>
+    </div>
+    <div class="sticky-note red">
+      <span class="tape"></span>
+      <div class="sticky-note-inner">
+        <p>You build on failure. You use it as a stepping stone. Close the door on the past. You don't try to forget the <strong>mistakes</strong>, but you don't dwell on it. You don't let it have any of your energy, or any of your time, or any of your space.</p>
+        <span class="author">Johnny Cash</span>
+      </div>
+    </div>
+    <div class="sticky-note green">
+      <span class="pin"></span>
+      <div class="sticky-note-inner">
+        <p>Today I choose life. Every morning when I wake up I can choose joy, happiness, negativity, pain... To feel the freedom that comes from being able to continue to make <strong>mistakes</strong> and choices - today I choose to feel life, not to deny my humanity but embrace it.</p>
+        <span class="author">Kevyn Aucoin</span>
+      </div>
+    </div>
+    <div class="sticky-note blue">
+      <span class="tape"></span>
+      <div class="sticky-note-inner">
+        <p>If you're not making <strong>mistakes</strong>, then you're not doing anything. I'm positive that a doer makes <strong>mistakes</strong>.</p>
+        <span class="author">John Wooden</span>
+      </div>
+    </div>
+    <div class="sticky-note purple">
+      <span class="pin"></span>
+      <div class="sticky-note-inner">
+        <p>I have made terrible <strong>mistakes</strong> that have hurt the people that I cared about the most, and I am terribly sorry. I am deeply ashamed of my terrible judgment and my actions.</p>
+        <span class="author">Anthony Weiner</span>
+      </div>
+    </div>
+    <div class="sticky-note pink">
+      <span class="pin"></span>
+      <div class="sticky-note-inner">
+        <p><strong>Mistakes</strong> are always forgivable, if one has the courage to admit them.</p>
+        <span class="author">Bruce Lee</span>
+      </div>
+    </div>
+    <div class="sticky-note orange">
+      <span class="tape"></span>
+      <div class="sticky-note-inner">
+        <p>Creativity is allowing yourself to make <strong>mistakes</strong>. Art is knowing which ones to keep.</p>
+        <span class="author">Scott Adams</span>
+      </div>
+    </div>
+    <div class="sticky-note cyan">
+      <span class="tape"></span>
+      <div class="sticky-note-inner">
+        <p>There are only two <strong>mistakes</strong> one can make along the road to truth; not going all the way, and not starting.</p>
+        <span class="author">Buddha</span>
+      </div>
+    </div>
+    
+    <!--twitter follow button-->
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 </body>
 </html>
---
+++ index.wip1.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html data-language="en_US" lang="en">
+<head>
+	<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="The greatest ideas come by mistake!">
+  <meta name="author" content="by-mistake">
+  <link rel="icon" type="image/png" href="http://res.cloudinary.com/by-mistake-com/image/upload/v1452344232/logo.by-mistake.v3.2_32x32_fnk8fl.png">
+	<title>The greatest ideas come by mistake!</title>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Allura' rel='stylesheet' type='text/css'>
+	<!-- custom styles -->
+	<style type="text/css">
+		@media (min-width: 320px) {
+		  body{
+		  	background: url('http://res.cloudinary.com/by-mistake-com/image/upload/c_scale,h_320,w_480/a_90/v1451035454/wip-bg_vvwfel.jpg') no-repeat center center fixed;
+		  	-webkit-background-size: cover;
+		    -moz-background-size: cover;
+		    -o-background-size: cover;
+		    background-size: cover;
+			color:#fff;
+		  	background-color:#333;
+		  	font-family: 'Open Sans',Arial,Helvetica,Sans-Serif;
+		  	height: 100%;
+		  }
+		  p.blocktext {
+        margin-left: auto;
+        margin-right: auto;
+        width: 7em;
+        font-family: 'Allura', cursive;
+        font-size: 24px;
+        color: #919198;
+        margin-top: 21px;
+      }
+		}
+		@media (min-width: 480px) {
+		  body{
+		  	background: url('http://res.cloudinary.com/by-mistake-com/image/upload/c_scale,h_320,w_480/a_0/v1451035454/wip-bg_vvwfel.jpg') no-repeat center center fixed;
+		  	-webkit-background-size: cover;
+		    -moz-background-size: cover;
+		    -o-background-size: cover;
+		    background-size: cover;
+			  color:#fff;
+		  	background-color:#333;
+		  	font-family: 'Open Sans',Arial,Helvetica,Sans-Serif;
+		  	height: 100%;
+		  }
+		}
+
+		@media (min-width: 667px) {
+		  body{
+		  	background: url('http://res.cloudinary.com/by-mistake-com/image/upload/w_768/wip-bg_vvwfel.jpg') no-repeat center center fixed;
+		  	-webkit-background-size: cover;
+		    -moz-background-size: cover;
+		    -o-background-size: cover;
+		    background-size: cover;
+			color:#fff;
+		  	background-color:#333;
+		  	font-family: 'Open Sans',Arial,Helvetica,Sans-Serif;
+		  	height: 100%;
+		  }
+		}
+
+		@media (min-width: 992px) {
+		  body{
+		  	background: url('http://res.cloudinary.com/by-mistake-com/image/upload/w_992/wip-bg_vvwfel.jpg') no-repeat center center fixed;
+		  	-webkit-background-size: cover;
+		    -moz-background-size: cover;
+		    -o-background-size: cover;
+		    background-size: cover;
+			  color:#fff;
+		  	background-color:#333;
+		  	font-family: 'Open Sans',Arial,Helvetica,Sans-Serif;
+		  	height: 100%;
+		  }
+		  p.blocktext {
+        margin-left: auto;
+        margin-right: auto;
+        width: 7em;
+        font-family: 'Allura', cursive;
+        font-size: 57px;
+        color: #919198;
+      }
+		}
+	</style>
+</head>
+<body>
+	<div class="content">
+	  <p class="blocktext">Work in progress...</p>
+	</div>
+
+<!-- scripts -->
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+<!-- end of scripts -->
+</body>
+</html>
